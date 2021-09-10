927b8b689e42c1085149928202f342bd6626e0b4
Guilherme <47395034+guig3@users.noreply.github.com>
2021-09-10 14:31:36 -0300

    NEW API FREE - YouTube to MP3 and MP4
    Source: https://api.tubemp3.biz
    Demo on: https://tubemp3.biz

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!-- YouTube to MP3 Converter embed code  -->
 
-<iframe class="button-api-frame" src="https://baixaryoutube.net/@api/button/mp3/CevxZvSJLk8" width="100%" height="100%" allowtransparency="true" scrolling="no" style="border:none"></iframe>
+<iframe class="button-api-frame" src="https://api.tubemp3.biz/mp3/CevxZvSJLk8" width="100%" height="100%" allowtransparency="true" scrolling="no" style="border:none"></iframe>
 
 <!-- Optional script that automatically makes iframe content responsive. -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/3.5.14/iframeResizer.min.js"></script>
@@ -10,7 +10,7 @@
 
 <!-- YouTube to MP4/Video Converter embed code  -->
 
-<iframe class="button-api-frame" src="https://baixaryoutube.net/@api/button/videos/CevxZvSJLk8" width="100%" height="100%" allowtransparency="true" scrolling="no" style="border:none"></iframe>
+<iframe class="button-api-frame" src="https://api.tubemp3.biz/video/CevxZvSJLk8" width="100%" height="100%" allowtransparency="true" scrolling="no" style="border:none"></iframe>
 
 <!-- Optional script that automatically makes iframe content responsive. -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/3.5.14/iframeResizer.min.js"></script>
