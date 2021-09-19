33f5a9b7c44f0cfba05b5a65d2c10bd4ccf9a021
Guilherme <47395034+guig3@users.noreply.github.com>
2021-09-19 15:23:50 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,17 @@
-<!-- YouTube to MP3 Converter embed code  -->
-
-<iframe class="button-api-frame" src="https://baixaryoutube.net/@api/button/mp3/CevxZvSJLk8" width="100%" height="100%" allowtransparency="true" scrolling="no" style="border:none"></iframe>
+VERSION V2
+<!-- YouTube to MP3 Converter embed code - v2  -->
+<iframe class="button-api-frame" src="https://api.tubemp3.biz/mp3/kTJczUoc26U" width="100%" height="100%" allowtransparency="true" scrolling="no" style="border:none"></iframe>
 
 <!-- Optional script that automatically makes iframe content responsive. -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/3.5.14/iframeResizer.min.js"></script>
-<script>iFrameResize({}, '.button-api-frame');</script>
+<script>iFrameResize({checkOrigin: false}, '.button-api-frame');</script>
 
-<br />
+VERSION V1
+<!-- YouTube to MP3 Converter embed code - v1  -->
+<iframe src="https://api.tubemp3.biz/audio/kTJczUoc26U" frameborder="0" style="width: 160px; height: 60px;"></iframe>
 
 <!-- YouTube to MP4/Video Converter embed code  -->
+<iframe src="https://api.tubemp3.biz/video/kTJczUoc26U" frameborder="0" style="width: 160px; height: 60px;"></iframe>
 
-<iframe class="button-api-frame" src="https://baixaryoutube.net/@api/button/videos/CevxZvSJLk8" width="100%" height="100%" allowtransparency="true" scrolling="no" style="border:none"></iframe>
 
-<!-- Optional script that automatically makes iframe content responsive. -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/iframe-resizer/3.5.14/iframeResizer.min.js"></script>
-<script>iFrameResize({}, '.button-api-frame');</script>
-
-<!-- Pleasse replace CevxZvSJLk8 with your own YouTube Video ID -->
+<!-- Pleasse replace kTJczUoc26U with your own YouTube Video ID -->
